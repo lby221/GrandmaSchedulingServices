0d8026152592a509939142886190a098ff7227a7
Baiyu Liu <berry2212@gmail.com>
2016-06-15 14:54:41 -0700

    Minor changes

--- src/distributor/email.go
+++ src/distributor/email.go
@@ -5,11 +5,11 @@ import (
 )
 
 func sendEmailMsg(endpoint string, message string) error {
-	auth := smtp.PlainAuth("", "tech@sellyx.com", "sTech123", "smtp.gmail.com")
+	auth := smtp.PlainAuth("", "email", "sTech123", "smtp.gmail.com")
 
 	to := []string{endpoint}
 	msg := []byte("To: " + endpoint + "\r\n" + message)
-	err := smtp.SendMail("smtp.gmail.com:587", auth, "donotreply@sellyx.com", to, msg)
+	err := smtp.SendMail("email", auth, "toemail", to, msg)
 	if err != nil {
 		return err
 	}
